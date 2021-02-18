7f96ac0198e1a3a80629b2635d94b75065a54913
ysthakur <2004yst@gmail.com>
2021-02-18 15:33:07 -0500

    Try ConditionalCommandMixin

--- src/main/java/org/usfirst/frc/team449/robot/mixIn/ConditionalCommandMixIn.java
+++ src/main/java/org/usfirst/frc/team449/robot/mixIn/ConditionalCommandMixIn.java
@@ -7,17 +7,18 @@ import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import java.util.function.BooleanSupplier;
 
 /**
- * A mix-in for {@link Command} that annotates its constructor for
- * use with Jackson. Don't make sublasses of this.
+ * A mix-in for {@link Command} that annotates its constructor for use with Jackson. Don't make
+ * sublasses of this.
  */
 public abstract class ConditionalCommandMixIn {
+
   /**
-   * @see ConditionalCommand#ConditionalCommand(Command,
-   *     Command, BooleanSupplier)
+   * @see ConditionalCommand#ConditionalCommand(Command, Command, BooleanSupplier)
    */
   @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
   public ConditionalCommandMixIn(
-      @JsonProperty(required = false) final Command onTrue,
-      @JsonProperty(required = false) final Command onFalse,
-      @JsonProperty(required = true) final BooleanSupplier condition) {}
+      @JsonProperty final Command onTrue,
+      @JsonProperty final Command onFalse,
+      @JsonProperty(required = true) final BooleanSupplier condition) {
+  }
 }
