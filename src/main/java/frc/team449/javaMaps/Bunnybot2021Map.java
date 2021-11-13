2e8f4160badfb6518f6febd1b5e070421fabc767
KiARC <katiearose@protonmail.com>
2021-11-13 15:57:37 -0500

    Modified Bunnybot2021Map to use the new SmartMotorConfigObject

--- src/main/java/frc/team449/javaMaps/Bunnybot2021Map.java
+++ src/main/java/frc/team449/javaMaps/Bunnybot2021Map.java
@@ -23,7 +23,7 @@ import frc.team449.generalInterfaces.shiftable.commands.ShiftGears;
 import frc.team449.jacksonWrappers.FeedForwardCalculators.MappedFeedForwardCalculator;
 import frc.team449.jacksonWrappers.*;
 import frc.team449.javaMaps.builders.PerGearSettingsBuilder;
-import frc.team449.javaMaps.builders.SmartMotorBuilder;
+import frc.team449.javaMaps.builders.SmartMotorConfigObject;
 import frc.team449.javaMaps.builders.ThrottlePolynomialBuilder;
 import frc.team449.oi.buttons.CommandButton;
 import frc.team449.oi.buttons.SimpleButton;
@@ -95,77 +95,71 @@ public class Bunnybot2021Map {
     var joysticks = List.of(mechanismsJoystick, driveJoystick);
 
     var navx = new MappedAHRS(SerialPort.Port.kMXP, true);
-
-    var driveMasterPrototype =
-        new SmartMotorBuilder()
-            .type(SmartMotor.Type.SPARK)
-            .enableBrakeMode(true)
-            .pdp(pdp)
-            .unitPerRotation(0.47877872)
-            .currentLimit(50)
-            .enableVoltageComp(true)
-            .startingGear(Shiftable.Gear.LOW)
-            .encoderCPR(256);
+    var driveMasterPrototype = new SmartMotorConfigObject()
+            .setType(SmartMotor.Type.SPARK)
+            .setEnableBrakeMode(true)
+            .setPdp(pdp)
+            .setUnitPerRotation(0.47877872)
+            .setCurrentLimit(50)
+            .setEnableVoltageComp(true)
+            .setStartingGear(Shiftable.Gear.LOW)
+            .setEncoderCPR(256);
     var lowGear =
-        new PerGearSettingsBuilder()
-            .gear(Shiftable.Gear.LOW)
-            .postEncoderGearing(0.0488998)
-            .maxSpeed(2.3)
-            .kP(0);
+            new PerGearSettingsBuilder()
+                    .gear(Shiftable.Gear.LOW)
+                    .postEncoderGearing(0.0488998)
+                    .maxSpeed(2.3)
+                    .kP(0);
     var highGear =
-        new PerGearSettingsBuilder()
-            .gear(Shiftable.Gear.HIGH)
-            .postEncoderGearing(0.12936611)
+            new PerGearSettingsBuilder()
+                    .gear(Shiftable.Gear.HIGH)
+                    .postEncoderGearing(0.12936611)
             .maxSpeed(5.2)
             .kP(0.000001);
 
     var leftMaster =
-        driveMasterPrototype
-            .copy()
-            .port(leftMasterPort)
-            .name("left")
-            .reverseOutput(true)
-            .slaveSparks(
-                List.of(
-                    new SlaveSparkMax(leftMasterSlave1Port, false, pdp),
-                    new SlaveSparkMax(leftMasterSlave2Port, false, pdp)))
-            .perGearSettings(
-                List.of(
-                    lowGear
-                        .feedForwardCalculator(new MappedFeedForwardCalculator(0.128, 5.23, 0.0698))
-                        .build(),
-                    highGear
-                        .feedForwardCalculator(new MappedFeedForwardCalculator(0.156, 2.01, 0.154))
-                        .build()))
-            .build();
+            SmartMotor.create(driveMasterPrototype
+                    .setPort(leftMasterPort)
+                    .setName("left")
+                    .setReverseOutput(true)
+                    .setSlaveSparks(
+                            List.of(
+                                    new SlaveSparkMax(leftMasterSlave1Port, false, pdp),
+                                    new SlaveSparkMax(leftMasterSlave2Port, false, pdp)))
+                    .setPerGearSettings(
+                            List.of(
+                                    lowGear
+                                            .feedForwardCalculator(new MappedFeedForwardCalculator(0.128, 5.23, 0.0698))
+                                            .build(),
+                                    highGear
+                                            .feedForwardCalculator(new MappedFeedForwardCalculator(0.156, 2.01, 0.154))
+                                            .build())));
     var rightMaster =
-        driveMasterPrototype
-            .copy()
-            .name("right")
-            .port(rightMasterPort)
-            .reverseOutput(false)
-            .slaveSparks(
-                List.of(
-                    new SlaveSparkMax(rightMasterSlave1Port, false, pdp),
-                    new SlaveSparkMax(rightMasterSlave2Port, false, pdp)))
-            .perGearSettings(
-                List.of(
-                    lowGear
-                        .feedForwardCalculator(new MappedFeedForwardCalculator(0.139, 5.17, 0.0554))
-                        .build(),
-                    highGear
-                        .feedForwardCalculator(new MappedFeedForwardCalculator(0.165, 2.01, 0.155))
-                        .build()))
-            .build();
+            SmartMotor.create(driveMasterPrototype
+                    .setName("right")
+                    .setPort(rightMasterPort)
+                    .setReverseOutput(false)
+                    .setSlaveSparks(
+                            List.of(
+                                    new SlaveSparkMax(rightMasterSlave1Port, false, pdp),
+                                    new SlaveSparkMax(rightMasterSlave2Port, false, pdp)))
+                    .setPerGearSettings(
+                            List.of(
+                                    lowGear
+                                            .feedForwardCalculator(new MappedFeedForwardCalculator(0.139, 5.17, 0.0554))
+                                            .build(),
+                                    highGear
+                                            .feedForwardCalculator(new MappedFeedForwardCalculator(0.165, 2.01, 0.155))
+                                            .build())));
     var drive =
-        new DriveUnidirectionalWithGyroShiftable(
-            leftMaster,
-            rightMaster,
-            navx,
-            0.61755,
-            new ShiftComponent(
-                List.of(leftMaster, rightMaster), new DoubleSolenoid(0, 0, 1), Shiftable.Gear.LOW),
-            false);
+            new DriveUnidirectionalWithGyroShiftable(
+                    leftMaster,
+                    rightMaster,
+                    navx,
+                    0.61755,
+                    new ShiftComponent(
+                            List.of(leftMaster, rightMaster), new DoubleSolenoid(0, 0, 1), Shiftable.Gear.LOW),
+                    false);
 
     // Elevator
     var elevatorPulleyMotor =
