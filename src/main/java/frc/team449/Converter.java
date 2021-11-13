b1b384f7c355f4284a958c2f635996da50bf2607
KiARC <katiearose@protonmail.com>
2021-11-13 17:17:36 -0500

    Make the joystick to velocity conversion quadratic
    I wrote this commit on my phone

--- src/main/java/frc/team449/Converter.java
+++ src/main/java/frc/team449/Converter.java
@@ -5,13 +5,13 @@ public class Converter {
     throw new IllegalStateException("Converter is a utility class!");
   }
   /**
-   * Converts a joystick input to a velocity in m/s by scaling it to the given scale.
+   * Converts a joystick input to a velocity in m/s by multiplying it by it's absolute value (to preserve negativity) and then scaling it to the given scale.
    *
    * @param input the joystick input to be converted
    * @param scale the scale to be applied (ex: the max elevator velocity)
    * @return the input scaled by the scale
    */
   public static double joystickInputToVelocity(double input, double scale) {
-    return input * scale;
+    return (input * Math.abs(input)) * scale;
   }
 }
