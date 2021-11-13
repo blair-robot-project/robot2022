355483d5c5f1d7b619443f969bf3300f6451aeed
KiARC <katiearose@protonmail.com>
2021-11-13 12:33:40 -0500

    SetVelocity now constantly runs and polls the joystick

--- src/main/java/frc/team449/_2021BunnyBot/Elevator/commands/SetVelocity.java
+++ src/main/java/frc/team449/_2021BunnyBot/Elevator/commands/SetVelocity.java
@@ -2,23 +2,26 @@ package frc.team449._2021BunnyBot.Elevator.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team449._2021BunnyBot.Elevator.OneMotorPulleyElevator;
+import frc.team449.jacksonWrappers.MappedJoystick;
 
 public class SetVelocity extends CommandBase {
     private final OneMotorPulleyElevator elevator;
     private final double velocity;
-    public SetVelocity(double velocity, OneMotorPulleyElevator elevator) {
+    private final MappedJoystick joystick;
+    public SetVelocity(double velocity, OneMotorPulleyElevator elevator, MappedJoystick joystick) {
         addRequirements(elevator);
         this.elevator = elevator;
         this.velocity = velocity;
+        this.joystick = joystick;
     }
 
     @Override
     public void execute(){
-        elevator.setVelocity(velocity);
+        elevator.setVelocity(joystick.getY());
     }
 
     @Override
     public boolean isFinished() {
-        return true;
+        return false;
     }
 }
