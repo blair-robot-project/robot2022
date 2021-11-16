b4626f0e941dcf03bb5f6192a1b05b4af3dc58f0
KiARC <katiearose@protonmail.com>
2021-11-16 17:45:54 -0500

    Added semicolon
    :|

--- src/main/java/frc/team449/generalInterfaces/simpleMotor/MappedVictorSPX.java
+++ src/main/java/frc/team449/generalInterfaces/simpleMotor/MappedVictorSPX.java
@@ -56,7 +56,7 @@ public class MappedVictorSPX implements SimpleMotor, Loggable {
     if (slaveVictors != null) {
       // Set up slaves.
       for (final SlaveVictor slave : slaveVictors) {
-        int i = voltageCompSamples != null ? voltageCompSamples : 32
+        int i = voltageCompSamples != null ? voltageCompSamples : 32;
         slave.setMaster(
             victorSPX,
             brakeMode,
