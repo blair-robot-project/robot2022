0af3f82f19bd77f83070bb0ba35725d5ba7b7717
Teddy <tewodros.mengistie@gmail.com>
2021-12-04 16:09:04 -0500

    Working on merge

--- src/main/java/frc/team449/javaMaps/Bunnybot2021Map.java
+++ src/main/java/frc/team449/javaMaps/Bunnybot2021Map.java
@@ -1,26 +1,26 @@
 package frc.team449.javaMaps;
 
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.team449.CommandContainer;
 import frc.team449.RobotMap;
 import frc.team449._2020.multiSubsystem.SolenoidSimple;
+import frc.team449._2020.multiSubsystem.commands.SetSolenoidPose;
 import frc.team449._2021BunnyBot.elevator.OneMotorPulleyElevator;
+import frc.team449._2021BunnyBot.elevator.OneMotorPulleyElevator.ElevatorPosition;
 import frc.team449._2021BunnyBot.elevator.commands.MoveToPosition;
-import frc.team449._2021BunnyBot.intake.OnePistonIntake;
+import frc.team449._2021BunnyBot.elevator.commands.SetVelocity;
 import frc.team449._2021BunnyBot.intake.commands.SetIntake;
 import frc.team449.components.RunningLinRegComponent;
 import frc.team449.components.ShiftComponent;
-import frc.team449.drive.unidirectional.DriveUnidirectionalWithGyro;
 import frc.team449.drive.unidirectional.DriveUnidirectionalWithGyroShiftable;
-import frc.team449.drive.unidirectional.commands.UnidirectionalNavXDefaultDrive;
 import frc.team449.generalInterfaces.SmartMotor;
 import frc.team449.generalInterfaces.doubleUnaryOperator.Polynomial;
-import frc.team449.generalInterfaces.doubleUnaryOperator.RampComponent;
 import frc.team449.generalInterfaces.shiftable.Shiftable;
+import frc.team449.generalInterfaces.shiftable.commands.ShiftGears;
 import frc.team449.jacksonWrappers.FeedForwardCalculators.MappedFeedForwardCalculator;
 import frc.team449.jacksonWrappers.*;
 import frc.team449.javaMaps.builders.PerGearSettingsBuilder;
@@ -31,7 +31,6 @@ import frc.team449.oi.buttons.SimpleButton;
 import frc.team449.oi.throttles.Throttle;
 import frc.team449.oi.throttles.ThrottleSum;
 import frc.team449.oi.unidirectional.arcade.OIArcadeWithDPad;
-import frc.team449.other.Debouncer;
 import frc.team449.other.DefaultCommand;
 import frc.team449.other.Updater;
 import org.jetbrains.annotations.NotNull;
@@ -40,45 +39,55 @@ import java.util.List;
 import java.util.Map;
 
 public class Bunnybot2021Map {
-  // Motor IDs
-  public static final int RIGHT_LEADER_PORT = 1;
-  public static final int RIGHT_LEADER_FOLLOWER_1_PORT = 2;
-  public static final int LEFT_LEADER_PORT = 3;
-  public static final int LEFT_LEADER_FOLLOWER_1_PORT = 4;
-  // Solenoid ports
-  public static final int INTAKE_SOLENOID_FORWARD_PORT = 2;
-  public static final int INTAKE_SOLENOID_REVERSE_PORT = 3;
-  // Controller ports
-  public static final int MECHANISMS_JOYSTICK_PORT = 0;
-  public static final int DRIVE_JOYSTICK_PORT = 1;
-  // Drive button numbers
-  public static final int SHIFT_TOGGLE_BUTTON = 5;
-  // Elevator stuff
-  private static final double ELEVATOR_MAX_VELOCITY = 5; // TODO this is a placeholder
-  private static final int ELEVATOR_MOTOR_PORT = 9;
-  private static final int ELEVATOR_MOVE_TO_TOP = 1;
-  private static final int ELEVATOR_MOVE_TO_UPPER = 2;
-  private static final int ELEVATOR_MOVE_TO_LOWER = 3;
-  private static final int ELEVATOR_MOVE_TO_BOTTOM = 4;
-  // Intake stuff
-  private static final int INTAKE_CLOSE = 7;
-  private static final int INTAKE_OPEN = 8;
-
   private Bunnybot2021Map() {
     throw new IllegalStateException("This is a utility class!");
   }
 
   @NotNull
   public static RobotMap createRobotMap() {
+    // TODO Declare these constants outside this method and remove unused variables
+
+    // Motor ports
+    int leftMasterPort = 1,
+        leftMasterSlave1Port = 3,
+        leftMasterSlave2Port = 5,
+        rightMasterPort = 2,
+        rightMasterSlave1Port = 4,
+        rightMasterSlave2Port = 6,
+        elevatorMotorPort = 8;
+
+    // Solenoid ports
+    int intakeSolenoidForward = 2, intakeSolenoidReverse = 3;
+
+    // Drive input-output ports. Things like encoders go here
+
+    // Joystick ports
+    int mechanismsJoystickPort = 0, driveJoystickPort = 1;
+
+    // Driver button numbers
+    int driverIntakeOutOn = 1,
+        driverIntakeOff = 2, // TODO This is never used
+        driverIntakeRev = 3, // TODO This is never used
+        driverIntakeInOff = 4, // TODO This is never used
+        shiftUp = 5;
+
+    // Mechs button numbers
+    int elevatorMoveToTop = 1,
+        elevatorMoveToUpper = 2,
+        elevatorMoveToLower = 3,
+        elevatorMoveToBottom = 4,
+        intakeClose = 7,
+        intakeOpen = 8;
+    // Motor speeds
+    double elevatorMaxVelocity = 1; // TODO this is a placeholder
+
     var useCameraServer = false;
+
     var pdp = new PDP(0, new RunningLinRegComponent(250, 0.75));
 
-    var driveJoystick = new MappedJoystick(DRIVE_JOYSTICK_PORT);
-    var mechanismsJoystick = new MappedJoystick(MECHANISMS_JOYSTICK_PORT);
-    var joysticks = List.of(driveJoystick, mechanismsJoystick);
-
-    var compressor = new Compressor();
-    var gearShiftingSolenoids = new DoubleSolenoid(0, 1, 0);
+    var mechanismsJoystick = new MappedJoystick(mechanismsJoystickPort);
+    var driveJoystick = new MappedJoystick(driveJoystickPort);
+    var joysticks = List.of(mechanismsJoystick, driveJoystick);
 
     var navx = new MappedAHRS(SerialPort.Port.kMXP, true);
     var driveMasterPrototype =
@@ -86,108 +95,113 @@ public class Bunnybot2021Map {
             .setType(SmartMotor.Type.SPARK)
             .setEnableBrakeMode(true)
             .setPdp(pdp)
-            .setUnitPerRotation(0.470799075)
+            .setUnitPerRotation(0.47877872)
             .setCurrentLimit(50)
             .setEnableVoltageComp(true)
-            .setStartingGear(Shiftable.Gear.HIGH);
+            .setStartingGear(Shiftable.Gear.LOW);
+    //            .setEncoderCPR(256);
     var lowGear =
         new PerGearSettingsBuilder()
             .gear(Shiftable.Gear.LOW)
-            .postEncoderGearing(1 / 20.45)
-            .maxSpeed(1.0); // in m/s
+            .postEncoderGearing(0.0488998)
+            .maxSpeed(2.3)
+            .kP(0);
     var highGear =
         new PerGearSettingsBuilder()
             .gear(Shiftable.Gear.HIGH)
-            .postEncoderGearing(1 / 7.73)
-            .maxSpeed(2.0); // in m/s
-    var rightMaster =
-        SmartMotor.create(
-            driveMasterPrototype
-                .setName("right")
-                .setPort(RIGHT_LEADER_PORT)
-                .setReverseOutput(false)
-                .setSlaveSparks(
-                    List.of(new SlaveSparkMax(RIGHT_LEADER_FOLLOWER_1_PORT, false, pdp)))
-                .setPerGearSettings(
-                    List.of(
-                        lowGear
-                            .feedForwardCalculator(
-                                new MappedFeedForwardCalculator(0.102, 5.66, 0.306)) // TODO characterize
-                            .build(),
-                        highGear
-                            .feedForwardCalculator(
-                                new MappedFeedForwardCalculator(
-                                    0.165, 2.01, 0.155)) // TODO characterize
-                            .build())));
+            .postEncoderGearing(0.12936611)
+            .maxSpeed(5.2)
+            .kP(0.000001);
+
     var leftMaster =
-        SmartMotor.create(
+        MappedSparkMax.create(
+            null,
+            null,
             driveMasterPrototype
-                .setPort(LEFT_LEADER_PORT)
+                .copy()
+                .setPort(leftMasterPort)
                 .setName("left")
                 .setReverseOutput(true)
-                .setSlaveSparks(List.of(new SlaveSparkMax(LEFT_LEADER_FOLLOWER_1_PORT, false, pdp)))
+                .setSlaveSparks(
+                    List.of(
+                        new SlaveSparkMax(leftMasterSlave1Port, false, pdp),
+                        new SlaveSparkMax(leftMasterSlave2Port, false, pdp)))
                 .setPerGearSettings(
                     List.of(
                         lowGear
                             .feedForwardCalculator(
-                                new MappedFeedForwardCalculator(0.102, 5.66, 0.306)) //TODO characterize
+                                new MappedFeedForwardCalculator(0.128, 5.23, 0.0698))
                             .build(),
                         highGear
                             .feedForwardCalculator(
-                                new MappedFeedForwardCalculator(
-                                    0.156, 2.01, 0.154)) // TODO characterize
-                            .build())));
-
+                                new MappedFeedForwardCalculator(0.156, 2.01, 0.154))
+                            .build()))
+                .build());
+    var rightMaster =
+        MappedSparkMax.create(
+            null,
+            null,
+            driveMasterPrototype
+                .copy()
+                .setName("right")
+                .setPort(rightMasterPort)
+                .setReverseOutput(false)
+                .setSlaveSparks(
+                    List.of(
+                        new SlaveSparkMax(rightMasterSlave1Port, false, pdp),
+                        new SlaveSparkMax(rightMasterSlave2Port, false, pdp)))
+                .setPerGearSettings(
+                    List.of(
+                        lowGear
+                            .feedForwardCalculator(
+                                new MappedFeedForwardCalculator(0.139, 5.17, 0.0554))
+                            .build(),
+                        highGear
+                            .feedForwardCalculator(
+                                new MappedFeedForwardCalculator(0.165, 2.01, 0.155))
+                            .build()))
+                .build());
     var drive =
         new DriveUnidirectionalWithGyroShiftable(
             leftMaster,
             rightMaster,
             navx,
-            0.61755, //TODO measure the distance from one side of the drive to the other
+            0.61755,
             new ShiftComponent(
-                List.of(leftMaster, rightMaster), gearShiftingSolenoids, Shiftable.Gear.LOW),
+                List.of(leftMaster, rightMaster), new DoubleSolenoid(0, 0, 1), Shiftable.Gear.LOW),
             false);
 
     // Elevator
-    // TODO setup elevator, with Meters
     var elevatorPulleyMotor =
-        new MappedSparkMax(
-            ELEVATOR_MOTOR_PORT,
-            "elevator",
-            false,
-            true,
-            pdp,
+        MappedSparkMax.create(
             null,
             null,
-            null,
-            null,
-            null,
-            1.0,
-            1.0, //TODO set to circumference of pulley for the elevator
-            40,
-            false,
-            List.of(new PerGearSettingsBuilder().gear(Shiftable.Gear.LOW).maxSpeed(2.0).build()),
-            Shiftable.Gear.LOW,
-            null,
-            null,
-            null,
-            null);
-    // PID constants for position controlled elevator motor
-    elevatorPulleyMotor.setPID(0, 0, 0); // TODO tune pid
+            new SmartMotorConfigBuilder()
+                .setName("elevator")
+                .setPort(elevatorMotorPort)
+                .setReverseOutput(false)
+                .setEnableBrakeMode(true)
+                .setPdp(pdp)
+                .setCurrentLimit(40)
+                .setEnableVoltageComp(false)
+                .build());
+    // PID constants for elevator
+    elevatorPulleyMotor.setPID(0, 0, 0);
     // WE ASSUME THE ELEVATOR STARTS AT THE BOTTOM
     // PLEASE MAKE SURE ELEVATOR IS ACTUALLY AT THE BOTTOM
 
-    var elevator = new OneMotorPulleyElevator(
-            elevatorPulleyMotor, OneMotorPulleyElevator.ElevatorPosition.BOTTOM);
+    var elevator =
+        new OneMotorPulleyElevator(
+            elevatorPulleyMotor, ElevatorPosition.BOTTOM, elevatorMaxVelocity);
+    var setVelocityCommand = new SetVelocity(elevator, mechanismsJoystick, elevatorMaxVelocity);
 
-    // Intake
+    // intake
     var intake =
-        new OnePistonIntake(
-            new SolenoidSimple(
-                new DoubleSolenoid(INTAKE_SOLENOID_FORWARD_PORT, INTAKE_SOLENOID_REVERSE_PORT)
-            )
-        );
-    // Controls for the drive
+        new IntakeActuated(
+            new SolenoidSimple(new DoubleSolenoid(intakeSolenoidForward, intakeSolenoidReverse)));
+
+    var subsystems = List.<Subsystem>of(drive, elevator, intake);
+
     var throttlePrototype =
         new ThrottlePolynomialBuilder().stick(driveJoystick).smoothingTimeSecs(0.04).scale(0.7);
     var rotThrottle =
@@ -228,70 +242,63 @@ public class Bunnybot2021Map {
             1.0,
             true);
 
-    var defaultDriveCommand =
-        new DefaultCommand(
-            drive,
-            new UnidirectionalNavXDefaultDrive<DriveUnidirectionalWithGyro>(
-                0,
-                new Debouncer(1.5),
-                0,
-                1.0,
-                null,
-                2,
-                3.0,
-                false,
-                0, // TODO tune pid
-                0,
-                0,
-                new Debouncer(0.15),
-                drive,
-                oi,
-                new RampComponent(3.0, 3.0)
-            )
-        );
+    var intakeSolenoid =
+        new SolenoidSimple(new DoubleSolenoid(intakeSolenoidForward, intakeSolenoidReverse));
 
-    var subsystems = List.<Subsystem>of(drive, elevator, intake);
+    var updater = new Updater(List.of(pdp, drive, oi, navx));
 
-    var updater = new Updater(List.of(pdp, oi, navx, drive));
-
-    var defaultCommands = List.of(defaultDriveCommand);
+    var defaultCommands = List.<DefaultCommand>of();
 
     var buttons =
         List.of(
+            // Run bumper and transition wheel
+            new CommandButton(
+                new SimpleButton(driveJoystick, driverIntakeOutOn),
+                new ParallelCommandGroup(
+                    new SetSolenoidPose(intakeSolenoid, DoubleSolenoid.Value.kForward)
+                    // todo more commands in this parallel command
+                    ),
+                CommandButton.Action.WHEN_PRESSED),
+            // Shift drive up *or* down
+            // todo Should there be 2 separate commands instead?
+            new CommandButton(
+                new SimpleButton(driveJoystick, shiftUp),
+                new ShiftGears(drive),
+                CommandButton.Action.WHEN_PRESSED),
             // elevator move to TOP position
             new CommandButton(
-                new SimpleButton(mechanismsJoystick, ELEVATOR_MOVE_TO_TOP),
-                new MoveToPosition(OneMotorPulleyElevator.ElevatorPosition.TOP, elevator),
+                new SimpleButton(mechanismsJoystick, elevatorMoveToTop),
+                new MoveToPosition(ElevatorPosition.TOP, elevator),
                 CommandButton.Action.WHEN_PRESSED),
             // elevator move to UPPER position
             new CommandButton(
-                new SimpleButton(mechanismsJoystick, ELEVATOR_MOVE_TO_UPPER),
-                new MoveToPosition(OneMotorPulleyElevator.ElevatorPosition.UPPER, elevator),
+                new SimpleButton(mechanismsJoystick, elevatorMoveToUpper),
+                new MoveToPosition(ElevatorPosition.UPPER, elevator),
                 CommandButton.Action.WHEN_PRESSED),
             // elevator move to LOWER position
             new CommandButton(
-                new SimpleButton(mechanismsJoystick, ELEVATOR_MOVE_TO_LOWER),
-                new MoveToPosition(OneMotorPulleyElevator.ElevatorPosition.LOWER, elevator),
+                new SimpleButton(mechanismsJoystick, elevatorMoveToLower),
+                new MoveToPosition(ElevatorPosition.LOWER, elevator),
                 CommandButton.Action.WHEN_PRESSED),
             // elevator move to BOTTOM position
             new CommandButton(
-                new SimpleButton(mechanismsJoystick, ELEVATOR_MOVE_TO_BOTTOM),
-                new MoveToPosition(OneMotorPulleyElevator.ElevatorPosition.BOTTOM, elevator),
+                new SimpleButton(mechanismsJoystick, elevatorMoveToBottom),
+                new MoveToPosition(ElevatorPosition.BOTTOM, elevator),
                 CommandButton.Action.WHEN_PRESSED),
             // Close the intake
             new CommandButton(
-                new SimpleButton(mechanismsJoystick, INTAKE_CLOSE),
-                new SetIntake(OnePistonIntake.IntakePosition.CLOSED, intake),
+                new SimpleButton(mechanismsJoystick, intakeClose),
+                new SetIntake(IntakeActuated.IntakePosition.CLOSED, intake),
                 CommandButton.Action.WHEN_PRESSED),
             // Open the intake
             new CommandButton(
-                new SimpleButton(mechanismsJoystick, INTAKE_OPEN),
-                new SetIntake(OnePistonIntake.IntakePosition.OPEN, intake),
+                new SimpleButton(mechanismsJoystick, intakeOpen),
+                new SetIntake(IntakeActuated.IntakePosition.OPEN, intake),
                 CommandButton.Action.WHEN_PRESSED));
 
     var robotStartupCommands = List.<Command>of();
     var autoStartupCommands = List.<Command>of();
-    var teleopStartupCommands = List.<Command>of();
+    var teleopStartupCommands = List.<Command>of(setVelocityCommand);
     var testStartupCommands = List.<Command>of();
     var allCommands =
         new CommandContainer(
