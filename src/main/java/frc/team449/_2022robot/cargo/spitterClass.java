6fa30713ab75bea49e683a24104c26b481be6a5f
sirswagger21 <77893563+sirswagger21@users.noreply.github.com>
2022-01-15 22:34:09 -0500

    Create spitterClass.java

---
+++ src/main/java/frc/team449/_2022robot/cargo/spitterClass.java
@@ -0,0 +1,11 @@
+package frc.team449._2022robot.cargo;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team449.generalInterfaces.SmartMotor;
+import org.jetbrains.annotations.NotNull;
+
+public class SpitterClass extends SubsystemBase {
+
+  #define motor/solenoid/etc. we will be using
+
+}
