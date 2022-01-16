dd4ab178896478885fb085324a9af866d432a925
ysthakur <45539777+ysthakur@users.noreply.github.com>
2022-01-16 16:32:53 -0500

    Rename spitterClass.java to Spitter.java

--- src/main/java/frc/team449/_2022robot/cargo/Spitter.java
+++ src/main/java/frc/team449/_2022robot/cargo/Spitter.java
@@ -1,7 +1,6 @@
 package frc.team449._2022robot.cargo;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.jetbrains.annotations.NotNull;
 
 public class Spitter extends SubsystemBase {
 
