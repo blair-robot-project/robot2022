ee57ca14d4fad41ed09e74f54c0472a31dd1c26e
sirswagger21 <77893563+sirswagger21@users.noreply.github.com>
2022-01-16 16:27:57 -0500

    Update src/main/java/frc/team449/_2022robot/cargo/spitterClass.java
    Co-authored-by: ysthakur <45539777+ysthakur@users.noreply.github.com>

--- src/main/java/frc/team449/_2022robot/cargo/spitterClass.java
+++ src/main/java/frc/team449/_2022robot/cargo/spitterClass.java
@@ -1,7 +1,6 @@
 package frc.team449._2022robot.cargo;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.team449.generalInterfaces.SmartMotor;
 import org.jetbrains.annotations.NotNull;
 
 public class SpitterClass extends SubsystemBase {
