9b58f1c7cb2260060bf04ea0a370eaf360a33a54
ysthakur <45539777+ysthakur@users.noreply.github.com>
2022-01-23 18:31:28 -0500

    Add basic cargo subsystem and buttons

---
+++ src/main/java/frc/team449/_2022robot/cargo/Cargo2022.java
@@ -0,0 +1,46 @@
+package frc.team449._2022robot.cargo;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Cargo2022 extends SubsystemBase {
+  /** The leader motor for the intake */
+  public final MotorController intakeMotor;
+  /** The top motor that lets balls be spit out */
+  public final MotorController spitterMotor;
+  /** The speed when intaking */
+  private final double intakeSpeed;
+  /** The speed of the spitter motor when spitting */
+  private final double spitterSpeed;
+
+  public Cargo2022(
+      MotorController intakeMotor,
+      MotorController spitterMotor,
+      double intakeSpeed,
+      double spitterSpeed) {
+    this.intakeMotor = intakeMotor;
+    this.spitterMotor = spitterMotor;
+    this.intakeSpeed = intakeSpeed;
+    this.spitterSpeed = spitterSpeed;
+  }
+
+  public void runIntake() {
+    intakeMotor.set(intakeSpeed);
+    spitterMotor.set(0);
+  }
+
+  public void runIntakeReverse() {
+    intakeMotor.set(-intakeSpeed);
+    spitterMotor.set(-spitterSpeed);
+  }
+
+  public void spit() {
+    intakeMotor.set(intakeSpeed);
+    spitterMotor.set(spitterSpeed);
+  }
+
+  public void stop() {
+    intakeMotor.set(0);
+    spitterMotor.set(0);
+  }
+}
