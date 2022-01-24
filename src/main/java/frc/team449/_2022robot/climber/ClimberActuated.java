0c23c6be8c2ca89ea2a08387c6441d1cfb3e032c
ysthakur <45539777+ysthakur@users.noreply.github.com>
2022-01-24 11:52:25 -0500

    Add limit switches to climber

--- src/main/java/frc/team449/_2022robot/climber/ClimberActuated.java
+++ src/main/java/frc/team449/_2022robot/climber/ClimberActuated.java
@@ -3,53 +3,80 @@ package frc.team449._2022robot.climber;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team449.jacksonWrappers.WrappedMotor;
 import frc.team449.multiSubsystem.SolenoidSimple;
 import org.jetbrains.annotations.NotNull;
 
 public class ClimberActuated extends ProfiledPIDSubsystem {
-    private final WrappedMotor telescopingArmWinch;
-    @NotNull private final double maxDistanceTelescope;
-    private final SolenoidSimple pivotingTelescopingArm;
-    private ElevatorFeedforward feedforward;
-    public ClimberActuated (@NotNull WrappedMotor telescopingArmWinch,
-                            @NotNull SolenoidSimple pivotingTelescopingArm,
-                            double maxDistanceTelescope,
-                            @NotNull ElevatorFeedforward feedforward,
-                            @NotNull double kP,
-                            @NotNull double kI,
-                            @NotNull double kD,
-                            @NotNull double telescopingArmMaxVelocity,
-                            @NotNull double telescopingArmMaxAcceleration){
-        super(new ProfiledPIDController(kP, kI, kD, new TrapezoidProfile.Constraints(telescopingArmMaxVelocity, telescopingArmMaxAcceleration)));
-        this.telescopingArmWinch = telescopingArmWinch;
-        this.pivotingTelescopingArm = pivotingTelescopingArm;
-        this.maxDistanceTelescope = maxDistanceTelescope;
-        this.feedforward = feedforward;
-    }
-    public void extendTelescopingArm(){
+  private final WrappedMotor telescopingArmWinch;
+  private final double maxDistanceTelescope;
+  private final double telescopingArmMaxVel;
+  private final SolenoidSimple pivotingTelescopingArm;
+  private final ElevatorFeedforward feedforward;
+  private final DigitalInput topLimitSwitch;
+  private final DigitalInput bottomLimitSwitch;
 
-    }
-    public void retractTelescopingArm(){
+  public ClimberActuated(
+      @NotNull WrappedMotor telescopingArmWinch,
+      @NotNull SolenoidSimple pivotingTelescopingArm,
+      @NotNull DigitalInput topLimitSwitch,
+      @NotNull DigitalInput bottomLimitSwitch,
+      double maxDistanceTelescope,
+      @NotNull ElevatorFeedforward feedforward,
+      double kP,
+      double kI,
+      double kD,
+      double telescopingArmMaxVelocity,
+      double telescopingArmMaxAcceleration) {
+    super(
+        new ProfiledPIDController(
+            kP,
+            kI,
+            kD,
+            new TrapezoidProfile.Constraints(
+                telescopingArmMaxVelocity, telescopingArmMaxAcceleration)));
+    this.telescopingArmWinch = telescopingArmWinch;
+    this.pivotingTelescopingArm = pivotingTelescopingArm;
+    this.maxDistanceTelescope = maxDistanceTelescope;
+    this.feedforward = feedforward;
+    this.topLimitSwitch = topLimitSwitch;
+    this.bottomLimitSwitch = bottomLimitSwitch;
+    this.telescopingArmMaxVel = telescopingArmMaxVelocity;
+  }
 
-    }
-    public void pivotTelescopingArmOut(){
+  public void extendTelescopingArm() {
+    // todo use a lower velocity than the max velocity
+    telescopingArmWinch.set(telescopingArmMaxVel);
+    while (!topLimitSwitch.get()) {}
+    telescopingArmWinch.set(0);
+  }
 
-    }
-    public void pivotTelescopingArmIn(){
+  public void retractTelescopingArm() {
+    // todo use a lower velocity than the max velocity
+    telescopingArmWinch.set(-telescopingArmMaxVel);
+    while (!bottomLimitSwitch.get()) {}
+    telescopingArmWinch.set(0);
+  }
 
-    }
+  public void pivotTelescopingArmOut() {
+    pivotingTelescopingArm.setSolenoid(DoubleSolenoid.Value.kForward);
+  }
 
-    @Override
-    protected void useOutput(double output, TrapezoidProfile.State setpoint) {
-        double feedForward = feedforward.calculate(setpoint.position, setpoint.velocity);
-        telescopingArmWinch.setVoltage(output + feedForward);
-    }
+  public void pivotTelescopingArmIn() {
+    pivotingTelescopingArm.setSolenoid(DoubleSolenoid.Value.kReverse);
+  }
 
-    @Override
-    protected double getMeasurement() {
-        return telescopingArmWinch.encoder.getPosition();
-    }
+  @Override
+  protected void useOutput(double output, TrapezoidProfile.State setpoint) {
+    double feedForward = feedforward.calculate(setpoint.position, setpoint.velocity);
+    telescopingArmWinch.setVoltage(output + feedForward);
+  }
+
+  @Override
+  protected double getMeasurement() {
+    return telescopingArmWinch.encoder.getPosition();
+  }
 }
