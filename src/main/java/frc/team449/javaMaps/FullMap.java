631410702e0240b4535711fd48f0c4c342df3d57
ysthakur <45539777+ysthakur@users.noreply.github.com>
2022-01-30 19:49:34 -0500

    Merge dev into climber

--- src/main/java/frc/team449/javaMaps/FullMap.java
+++ src/main/java/frc/team449/javaMaps/FullMap.java
@@ -1,31 +1,18 @@
 package frc.team449.javaMaps;
 
 import edu.wpi.first.math.controller.ElevatorFeedforward;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.team449.CommandContainer;
 import frc.team449.RobotMap;
-import frc.team449._2022robot.cargo.Cargo2022;
 import frc.team449._2022robot.climber.PivotingTelescopingClimber;
 import frc.team449._2022robot.climber.commands.ExtendTelescopingArm;
 import frc.team449._2022robot.climber.commands.RetractTelescopingArm;
 import frc.team449.components.RunningLinRegComponent;
-import frc.team449.drive.unidirectional.DriveUnidirectionalWithGyro;
-import frc.team449.drive.unidirectional.commands.UnidirectionalNavXDefaultDrive;
-import frc.team449.generalInterfaces.doubleUnaryOperator.Polynomial;
-import frc.team449.generalInterfaces.doubleUnaryOperator.RampComponent;
-import frc.team449.javaMaps.builders.DriveSettingsBuilder;
 import frc.team449.javaMaps.builders.SparkMaxConfig;
-import frc.team449.javaMaps.builders.ThrottlePolynomialBuilder;
-import frc.team449.oi.buttons.SimpleButton;
-import frc.team449.oi.throttles.ThrottleSum;
-import frc.team449.oi.unidirectional.arcade.OIArcadeWithDPad;
-import frc.team449.other.Debouncer;
 import frc.team449.other.DefaultCommand;
-import frc.team449.other.FollowerUtils;
 import frc.team449.other.Updater;
 import frc.team449.wrappers.AHRS;
 import frc.team449.wrappers.PDP;
@@ -33,8 +20,6 @@ import frc.team449.wrappers.RumbleableJoystick;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class FullMap {
   // Motor IDs
@@ -67,118 +52,16 @@ public class FullMap {
 
     var navx = new AHRS(SerialPort.Port.kMXP, true);
 
-    var driveMasterPrototype =
+    var sparkPrototype =
         new SparkMaxConfig()
             .setEnableBrakeMode(true)
             .setUnitPerRotation(0.4787787204060999)
             .setCurrentLimit(50)
             .setEnableVoltageComp(true);
-    var rightMaster =
-        driveMasterPrototype
-            .copy()
-            .setName("right")
-            .setPort(RIGHT_LEADER_PORT)
-            .setReverseOutput(false)
-            .addSlaveSpark(FollowerUtils.createFollowerSpark(RIGHT_LEADER_FOLLOWER_1_PORT), false)
-            .createReal();
-    var leftMaster =
-        driveMasterPrototype
-            .copy()
-            .setPort(LEFT_LEADER_PORT)
-            .setName("left")
-            .setReverseOutput(true)
-            .addSlaveSpark(FollowerUtils.createFollowerSpark(LEFT_LEADER_FOLLOWER_1_PORT), false)
-            .createReal();
-
-    var drive =
-        new DriveUnidirectionalWithGyro(
-            leftMaster,
-            rightMaster,
-            navx,
-            new DriveSettingsBuilder()
-                .postEncoderGearing(1 / 20.45)
-                .maxSpeed(2.3)
-                .leftFeedforward(new SimpleMotorFeedforward(0.24453, 5.4511, 0.7127))
-                .rightFeedforward(new SimpleMotorFeedforward(0.2691, 5.3099, 0.51261))
-                .build(),
-            0.61755);
-
-    var throttlePrototype =
-        new ThrottlePolynomialBuilder().stick(driveJoystick).smoothingTimeSecs(0.04).scale(0.7);
-    var rotThrottle =
-        throttlePrototype
-            .axis(0)
-            .deadband(0.08)
-            .inverted(false)
-            .polynomial(
-                new Polynomial(
-                    Map.of(
-                        1., 0.009,
-                        2., 0.002),
-                    null))
-            .build();
-    var fwdThrottle =
-        new ThrottleSum(
-            Set.of(
-                throttlePrototype
-                    .axis(3)
-                    .deadband(0.05)
-                    .inverted(true)
-                    .polynomial(
-                        new Polynomial(
-                            Map.of(
-                                1., 0.01,
-                                2., 0.06),
-                            null))
-                    .build(),
-                throttlePrototype.axis(2).inverted(false).build()));
-    var oi =
-        new OIArcadeWithDPad(
-            rotThrottle,
-            fwdThrottle,
-            0.1,
-            false,
-            driveJoystick,
-            new Polynomial(
-                Map.of(
-                    0.5, 0.4,
-                    0., 0.2),
-                null),
-            0.7,
-            true);
-
-    drive.setDefaultCommand(
-        new UnidirectionalNavXDefaultDrive<>(
-            0,
-            new Debouncer(1.5),
-            0,
-            1.0,
-            null,
-            2,
-            3.0,
-            false,
-            0,
-            0,
-            0,
-            new Debouncer(0.15),
-            drive,
-            oi,
-            new RampComponent(2.0, 2.0)));
-
-    var cargo =
-        new Cargo2022(
-            new SparkMaxConfig()
-                .setName("intakeMotor")
-                .setPort(INTAKE_LEADER_PORT)
-                .addSlaveSpark(FollowerUtils.createFollowerSpark(INTAKE_FOLLOWER_PORT), false)
-                .createReal(),
-            new SparkMaxConfig().setName("spitterMotor").setPort(SPITTER_PORT).createReal(),
-            INTAKE_SPEED,
-            SPITTER_SPEED);
 
     var climber =
         new PivotingTelescopingClimber(
-            driveMasterPrototype
+            sparkPrototype
                 .copy()
                 .setName("climber_motor")
                 .setPort(CLIMBER_MOTOR_PORT)
@@ -197,24 +80,9 @@ public class FullMap {
             );
 
     // PUT YOUR SUBSYSTEM IN HERE AFTER INITIALIZING IT
-    var subsystems = List.<Subsystem>of(drive, cargo, climber);
+    var subsystems = List.<Subsystem>of(climber);
 
     var updater = new Updater(List.of(pdp, navx));
-
-    // Button bindings here
-
-    // Take in balls but don't shoot
-    new SimpleButton(mechanismsJoystick, INTAKE_NORMAL_BUTTON)
-        .whileHeld(cargo::runIntake, cargo)
-        .whenReleased(cargo::stop, cargo);
-    // Run intake backwards so human can feed balls
-    new SimpleButton(mechanismsJoystick, INTAKE_REVERSE_BUTTON)
-        .whileHeld(cargo::runIntakeReverse, cargo)
-        .whenReleased(cargo::stop, cargo);
-    // Run all motors in intake to spit balls out
-    new SimpleButton(mechanismsJoystick, SPIT_BUTTON)
-        .whileHeld(cargo::spit, cargo)
-        .whenReleased(cargo::stop, cargo);
 
     var defaultCommands = List.<DefaultCommand>of();
 
