dbad82f15dcfc1f956f72d64d63050763f18df95
John Joseph <69046000+jpothen8@users.noreply.github.com>
2022-03-28 17:39:59 -0400

    Update FlywheelShooter.java

--- src/main/java/frc/team449/_2022robot/cargo/FlywheelShooter.java
+++ src/main/java/frc/team449/_2022robot/cargo/FlywheelShooter.java
@@ -1,3 +1,33 @@
 package frc.team449._2022robot.cargo;
 
-public class FlywheelShooter {
+import edu.wpi.first.math.controller.BangBangController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.jetbrains.annotations.NotNull;
+
+public class FlywheelShooter extends SubsystemBase {
+    public final MotorController flywheelMotor;
+    /** The desired speed of the flywheel motor */
+    private final double setpoint;
+    private final BangBangController controller = new BangBangController();
+    private final Counter encoder;
+    private final SimpleMotorFeedforward feedforward;
+
+    public FlywheelShooter(@NotNull MotorController flywheelMotor, double setpoint, Counter encoder, double kS, double kA, double kV) {
+        this.flywheelMotor = flywheelMotor;
+        this.setpoint = setpoint;
+        this.encoder = encoder;
+        this.feedforward = new SimpleMotorFeedforward(kS, kV, kA);
+    }
+
+    public void shoot() {
+        flywheelMotor.setVoltage(controller.calculate(encoder.getRate(), setpoint) * 12.0 + 0.9 * feedforward.calculate(setpoint));
+    }
+
+    public void stop() {
+        flywheelMotor.set(0);
+    }
+}
