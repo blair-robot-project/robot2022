b3fa813e124c1b3e689e8f679cdce51398f74f6b
Teddy <tewodros.mengistie@gmail.com>
2022-04-04 13:37:44 -0400

    Add three ball high auto building on stationtwoballhighauto

---
+++ src/main/java/frc/team449/robot2022/routines/ThreeBallHighStraightAuto.java
@@ -0,0 +1,41 @@
+package frc.team449.robot2022.routines;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.team449.ahrs.PIDAngleController;
+import frc.team449.drive.unidirectional.DriveUnidirectionalWithGyro;
+import frc.team449.robot2022.cargo.Cargo2022;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class ThreeBallHighStraightAuto {
+  public static final Pose2d start = StationTwoBallHighAuto.end;
+  public static final Pose2d ball = ThreeBallLowAuto.ball;
+  public static final Pose2d ballReversed =
+      AutoUtils.pose(ball.getX(), ball.getY(), ball.getRotation().getDegrees() + 180);
+  public static final Pose2d end = start;
+
+  public static Command createCommand(
+      @NotNull DriveUnidirectionalWithGyro drive,
+      @NotNull Cargo2022 cargo,
+      @NotNull Supplier<PIDAngleController> angleController,
+      @NotNull Supplier<TrajectoryConfig> trajConfig,
+      Field2d field) {
+    return new InstantCommand(cargo::deployIntake, cargo)
+        .andThen(
+            AutoUtils.getBallAndScoreHigh(
+                drive,
+                cargo,
+                angleController.get(),
+                trajConfig,
+                List.of(start, ball),
+                List.of(ballReversed, end),
+                ThreeBallHighStraightAuto.class.getSimpleName(),
+                field));
+  }
+}
