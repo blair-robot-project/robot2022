fda5260b0fadc0a1a6798d4e618d6069247b3ffa
ysthakur <45539777+ysthakur@users.noreply.github.com>
2022-04-04 20:47:07 -0400

    Make 3 ball have sharper turns

--- src/main/java/frc/team449/robot2022/routines/ThreeBallHighCurvyAuto.java
+++ src/main/java/frc/team449/robot2022/routines/ThreeBallHighCurvyAuto.java
@@ -4,7 +4,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.constraint.CentripetalAccelerationConstraint;
-import edu.wpi.first.math.trajectory.constraint.MaxVelocityConstraint;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -17,33 +16,32 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public class ThreeBallHighCurvyAuto {
-  private static final Pose2d start = AutoUtils.pose(7.56, 2.99, 180 - 112.55);
-  private static final Pose2d turnPoint = AutoUtils.pose(6.91, 1.81, 180 - 111.47);
-  private static final Pose2d ball2 = AutoUtils.pose(5.54, 2.3, -140.28);
-  private static final Pose2d between = AutoUtils.pose(5.75, 1.13, -37.50);
-  private static final Pose2d ball3 = AutoUtils.pose(7.13, 0.66, -5.00);
-  private static final Pose2d between2 = AutoUtils.pose(8.03, 1.16, 82.87);
-  public static final Pose2d end = start;
   public static final double MAX_CENTRIPETAL_ACCEL = 0.8;
+  private static final Pose2d start = AutoUtils.pose(7.56, 2.99, 180 - 112.55);
+  public static final Pose2d end = start;
+  private static final Pose2d turnPoint = AutoUtils.pose(6.83, 2.26, 180 - 79.38);
+  private static final Pose2d ball2 = AutoUtils.pose(5.52, 2.40, -129.29);
+  private static final Pose2d between1 = AutoUtils.pose(5.98, 1.27, -34.08);
+  private static final Pose2d ball3 = AutoUtils.pose(7.26, 0.83, -48.95);
+  private static final Pose2d between2 = AutoUtils.pose(8.11, 0.85, 67.89);
+  private static final Pose2d between3 = AutoUtils.pose(8.23, 1.75, 98.13);
 
   public static Command createCommand(
       @NotNull DriveUnidirectionalWithGyro drive,
       @NotNull Cargo2022 cargo,
       @NotNull Supplier<TrajectoryConfig> trajConfig,
       @NotNull Field2d field) {
-    var constraints =
-        List.of(
-            new CentripetalAccelerationConstraint(MAX_CENTRIPETAL_ACCEL));
+    var constraints = List.of(new CentripetalAccelerationConstraint(MAX_CENTRIPETAL_ACCEL));
     var turnTraj =
         TrajectoryGenerator.generateTrajectory(
             List.of(start, turnPoint),
             trajConfig.get().setReversed(true).addConstraints(constraints));
     var getBallsTraj =
         TrajectoryGenerator.generateTrajectory(
-            List.of(turnPoint, ball2, between, ball3, between2, end),
+            List.of(turnPoint, ball2, between1, ball3, between2, between3, end),
             trajConfig.get().setReversed(false).addConstraints(constraints));
     var fullTraj = turnTraj.concatenate(getBallsTraj);
-    field.getObject(ThreeBallLowAuto.class.getSimpleName()).setTrajectory(fullTraj);
+    field.getObject(ThreeBallHighCurvyAuto.class.getSimpleName()).setTrajectory(fullTraj);
     return AutoUtils.shootHighCommand(cargo)
         .andThen(
             new RamseteControllerUnidirectionalDrive(drive, fullTraj)
