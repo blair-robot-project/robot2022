241b67517b84ea98153354bab18f5a057cb2b9dd
John Joseph <jpothen8@gmail.com>
2022-04-05 22:51:39 -0400

    Added shooter command to HangarTwoBallHigh

--- src/main/java/frc/team449/robot2022/routines/HangarTwoBallHigh.java
+++ src/main/java/frc/team449/robot2022/routines/HangarTwoBallHigh.java
@@ -4,7 +4,6 @@ import com.pathplanner.lib.PathPlanner;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.team449.auto.commands.RamseteControllerUnidirectionalDrive;
