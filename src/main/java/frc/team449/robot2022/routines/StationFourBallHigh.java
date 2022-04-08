3efb10735541c1f3335c622e3401dbb755d9c90d
John <jpothen8@gmail.com>
2022-04-08 14:50:32 -0400

    renaming

--- src/main/java/frc/team449/robot2022/routines/StationFourBallHigh.java
+++ src/main/java/frc/team449/robot2022/routines/StationFourBallHigh.java
@@ -1,22 +1,17 @@
 package frc.team449.robot2022.routines;
 
 import com.pathplanner.lib.PathPlanner;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.team449.ahrs.PIDAngleController;
 import frc.team449.auto.commands.RamseteControllerUnidirectionalDrive;
 import frc.team449.drive.unidirectional.DriveUnidirectionalWithGyro;
-import frc.team449.drive.unidirectional.commands.NavXTurnToAngle;
 import frc.team449.robot2022.cargo.Cargo2022;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
 import java.util.function.Supplier;
 
-public class StationFourBallHighAuto {
+public class StationFourBallHigh {
     public static Command createCommand(
             @NotNull DriveUnidirectionalWithGyro drive,
             @NotNull Cargo2022 cargo,
