e6ca53bd910f3c2af1ef660a492d5d36e0a2a6a0
John <jpothen8@gmail.com>
2022-04-08 21:14:12 -0400

    Station 4 ball auto with parallel commands, will optimize path later

--- src/main/java/frc/team449/robot2022/routines/StationFourBallHigh.java
+++ src/main/java/frc/team449/robot2022/routines/StationFourBallHigh.java
@@ -1,11 +1,14 @@
 package frc.team449.robot2022.routines;
 
+import com.pathplanner.lib.PathPlanner;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.team449.ahrs.PIDAngleController;
 import frc.team449.auto.commands.RamseteControllerUnidirectionalDrive;
 import frc.team449.drive.unidirectional.DriveUnidirectionalWithGyro;
@@ -16,44 +19,35 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 import java.util.function.Supplier;
 
-public class StationFourBallHighAuto {
-  public static final Pose2d start = StationTwoBallHighStraightAuto.end;
-  public static final Pose2d midTurn = ThreeBallHighStraightAuto.mid;
-  public static final Pose2d midTurnRev = ThreeBallHighStraightAuto.midRev;
-  public static final Pose2d ball3 = new Pose2d(5.57, 1.91, Rotation2d.fromDegrees(-171.63));
-  public static final Pose2d ball4 =
-      AutoUtils.withAngle(
-          StationFourBallLowAuto.ball4,
-          -169.14); // new Pose2d(1.69, 1.15, Rotation2d.fromDegrees(180.0));
-  public static final Pose2d ball4Rev = AutoUtils.withAngle(ball4, -5);
-  public static final Pose2d end = start;
+public class StationFourBallHigh {
 
   public static Command createCommand(
-      @NotNull DriveUnidirectionalWithGyro drive,
-      @NotNull Cargo2022 cargo,
-      @NotNull Supplier<PIDAngleController> angleController,
-      @NotNull Supplier<TrajectoryConfig> trajConfig,
-      Field2d field) {
-    var reverseTraj =
-        TrajectoryGenerator.generateTrajectory(
-            List.of(start, midTurn), trajConfig.get().setReversed(true));
-    field.getObject("StationFourBallHighReverse").setTrajectory(reverseTraj);
-    return StationTwoBallHighStraightAuto.createCommand(
-            drive, cargo, angleController, trajConfig, field)
-        .andThen(new RamseteControllerUnidirectionalDrive(drive, reverseTraj, false))
-        .andThen(
-            new NavXTurnToAngle(
-                midTurnRev.getRotation().getDegrees(), 1, drive, angleController.get()))
-        .andThen(
-            AutoUtils.getBallAndScoreHigh(
-                drive,
-                cargo,
-                angleController.get(),
-                trajConfig,
-                List.of(midTurnRev, ball3, ball4),
-                List.of(ball4Rev, end),
-                "StationFourBallAuto",
-                field,
-                false));
+          @NotNull DriveUnidirectionalWithGyro drive,
+          @NotNull Cargo2022 cargo,
+          @NotNull Supplier<TrajectoryConfig> trajConfig,
+          Field2d field) {
+    final double accel = 2.85;
+    var dump1 =
+            PathPlanner.loadPath(
+                    "Station 4Ball High Pt1", AutoConstants.AUTO_MAX_SPEED, accel);
+    field.getObject("Station 4Ball High Pt1").setTrajectory(dump1);
+    var dump2 =
+            PathPlanner.loadPath(
+              "Station 4Ball High Pt2", AutoConstants.AUTO_MAX_SPEED, accel);
+    field.getObject("Station 4Ball High Pt2").setTrajectory(dump2);
+    var traj =
+            PathPlanner.loadPath(
+                    "Station 4Ball High", AutoConstants.AUTO_MAX_SPEED, accel);
+    field.getObject("Station 4Ball High").setTrajectory(traj);
+    return new RamseteControllerUnidirectionalDrive(drive, traj)
+            .alongWith(
+                    new InstantCommand(cargo::deployIntake)
+                            .andThen(cargo::runIntake)
+                            .andThen(cargo::deployHood)
+                            .andThen(new WaitCommand(dump1.getTotalTimeSeconds() - 0.7))
+                            .andThen(cargo::startShooterCommand)
+                            .andThen(new WaitCommand(dump2.getTotalTimeSeconds() - 0.7))
+                            .andThen(cargo::startShooterCommand)
+            );
   }
 }
